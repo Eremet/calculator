5bdf4b92de14641378fc007679cbb3b3f91771b1
Eremet <eremetbekeshev@gmail.com>
2021-01-08 20:35:08 +0600

    calculator on react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+        const [input, setInput] = useState('')
+        const calcBtns = [];
+        [9,8,7,6,5,4,3,2,1,0,'.', '%'].forEach( (item) =>{
+          calcBtns.push(
+            <button
+            onClick={(e)=>{
+              setInput(input + e.target.value)
+            }}
+            value={item}
+            key={item}
+            >
+              {''}
+              {item}
+            </button>
+          )
+        })
+
+  return (
+    <div className="wrapper">
+      {''}
+      <div className="show-input">{input}</div>
+      <div className="digits flex">{calcBtns}</div>
+      <div className="modifiers subgrid">
+
+        {/* clear button */}
+        <button
+        onClick={()=> setInput(input.substr(0, input.length -1))}
+        >
+        Clear
+        </button>
+
+        {/* clear All */}
+
+        <button
+          onClick={(e) => setInput('')}
+          value=''
+        >
+          AC
+        </button>
+      </div>
+      <div className="operations subgrid">
+        <button
+        onClick={(e)=> setInput(input + e.target.value)}
+        value='+'
+        >
+          +
+        </button>
+
+        <button
+        onClick={(e)=> setInput(input + e.target.value)}
+        value='-'
+        >
+          -
+        </button>
+        <button
+        onClick={(e)=> setInput(input + e.target.value)}
+        value='*'
+        >
+          *
+        </button>
+        <button
+        onClick={(e)=> setInput(input + e.target.value)}
+        value='/'
+        >
+          /
+        </button>
+
+        <button
+        onClick={(e)=> setInput(input + e.target.value)}
+        value='%'
+        >
+          %
+        </button>
+
+        {/* "=" button */}
+
+        <button
+          onClick={e => {
+            try {
+              setInput(
+                  String(eval(input)).length > 3 &&
+                  String(eval(input)).includes('.')
+                  ? String(eval(input).toFixed(3))
+                  : String(eval(input))
+              )
+            }catch (e) {
+              console.log(e)
+            }
+          }}
+        >
+        =
+        </button>
+
+      </div>
+    </div>
+  )
+}
+export default App
